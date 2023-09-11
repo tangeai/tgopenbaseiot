9f81bd70a1274cc9a64379e75b15c555e6e72de3
liubin <liubin361163@163.com>
2023-09-11 18:14:03 +0800

    1.3.7

--- ios/TGBaseIOT.framework/Headers/TGBaseIOTDefine.h
+++ ios/TGBaseIOT.framework/Headers/TGBaseIOTDefine.h
@@ -141,7 +141,7 @@ typedef NS_ENUM(NSInteger, TGDevicePTZStyle) {
 #define TGBaseIOT_RelativeHeightX(value)            (TGBaseIOT_SCREENHEIGHT * value / 812.0f)
 #define TGBaseIOT_OEMSVGIconSize50                  (CGSizeMake(TGBaseIOT_RelativeHeightX(50), TGBaseIOT_RelativeHeightX(50)))
 
-#define TGBaseIOTSDKVersion                         @"1370"
+#define TGBaseIOTSDKVersion                         @"608"
 #define TG_CurrentLogLevel                          @"TG_CurrentLogLevel"
 
 #define TG_CameraZoomDefaultStep                    50
