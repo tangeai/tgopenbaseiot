cbc8d226c5006cd07b243068b2dd717b185a4668
liubin <liubin361163@163.com>
2024-01-26 14:19:13 +0800

    优化降噪能力

--- ios/Classes/TGBaseIOT.framework/Headers/TGCameraDeviceModel.h
+++ ios/Classes/TGBaseIOT.framework/Headers/TGCameraDeviceModel.h
@@ -9,7 +9,6 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #import "TGBaseIOTDefine.h"
-//#import "TGDeviceCapacityModel.h"
 #import <DAAudioVideo/DAAudioVideo.h>
 
 NS_ASSUME_NONNULL_BEGIN
@@ -20,18 +19,33 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, assign) BOOL isBinded;
 
 #pragma mark - listData
-@property (nonatomic, copy) NSString *deviceId;
-@property (nonatomic, copy) NSString *deviceName;
-@property (nonatomic, copy) NSString *productName;
-@property (nonatomic, strong) NSString *categoryName;
-@property (nonatomic, strong) NSString *categoryKey;
-@property (nonatomic, strong) NSString *connectWay;
+// 是否设备的主人
 @property (nonatomic, assign) BOOL is_owner;
-@property (nonatomic, strong) NSString *p2pType;
+// 设备ID
+@property (nonatomic, copy) NSString *deviceId;
+// 设备昵称
+@property (nonatomic, copy) NSString *deviceName;
+// 设备联网方式，例如：lan(有线连接)、wifi、sim(4G卡)
+@property (nonatomic, copy) NSString *connectWay;
+// 设备产品标识
+@property (nonatomic, copy) NSString *productKey;
+// 设备归属产品名称，例如：越南IPC
+@property (nonatomic, copy) NSString *productName;
+// 设备品类标识，例如：ipc
+@property (nonatomic, copy) NSString *categoryKey;
+// 设备品类，例如：普通IPC
+@property (nonatomic, copy) NSString *categoryName;
 
+// 设备属性，例如："{\"category\":\"ipc\",\"network\":\"wifi\",\"os\":\"linux\"}"
 @property (nonatomic, strong) NSDictionary *attrs;
+// 设备能力集，例如："{\"Microphone\":\"Yes\",\"RecordConf\":\"res\"}"
 @property (nonatomic, strong) NSDictionary *abilities;
+// 设备扩展属性，例如："{\"model\":\"TG-IPC-001\"}"
 @property (nonatomic, strong) NSDictionary *extAttrs;
+// 设备AI能力集，例如："{\"hw_ai\":[{\"flags\":1,\"type\":\"bd\"}],\"sw_ai\":[\"mt\"]}"
+@property (nonatomic, strong) NSDictionary *aiAbilities;
+// 设备属性扩展黑盒，可以是任意数据格式
+@property (nonatomic, strong) id extends;
 
 #pragma mark - timezone
 
@@ -58,41 +72,40 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark - connectInfor
 
 @property (nonatomic, copy) NSString *connectInfor;
-@property (nonatomic, copy) NSString *p2p_platform;
-@property (nonatomic, copy) NSString *p2pId;
 @property (nonatomic, copy) NSString *password;
 @property (nonatomic, copy) NSString *relay;
-@property (nonatomic, copy) NSString *webrtc;
+@property (nonatomic, copy) NSString *p2pId;
+@property (nonatomic, copy) NSString *p2pType;
+@property (nonatomic, copy) NSString *p2p_platform;
 @property (nonatomic, copy) NSString *p2pStateServer;
 @property (nonatomic, strong) NSArray *webrtcInfo;
 
 #pragma mark - coreImage
 
-//@property (nonatomic, strong) UIImage *coverImage;
 @property (nonatomic, copy) NSString *imagePath;
 @property (nonatomic, copy) NSString *imgTimeStamp;
 
-#pragma mark - des_key
+#pragma mark - used info
 
-@property (nonatomic, strong) NSString *des_key;
-
+@property (nonatomic, strong) NSMutableDictionary *deviceInfo;
+@property (nonatomic, strong) NSMutableDictionary *featureInfo;
+@property (nonatomic, strong) NSMutableDictionary *customInfo;
 //@property (nonatomic, strong) TGDeviceCapacityModel *capacityModel;
 
 #pragma mark - other 以下参数均为定制兼容请勿使用，如需使用请自己实现其方法
 
+@property (nonatomic, strong) NSString *des_key;
 @property (nonatomic, strong) NSString *ipAddress;
-@property (nonatomic, copy, nullable) void(^deviceUpdateProgress)(void);
 @property (nonatomic, copy) NSString *deviceType;
 @property (nonatomic, assign) BOOL isPreConnect;
-@property (nonatomic, copy) NSString *speed;
-@property (nonatomic, strong) NSMutableDictionary *deviceInfo;
-@property (nonatomic, strong) NSMutableDictionary *featureInfo;
-@property (nonatomic, strong) NSMutableDictionary *customInfo;
+@property (nonatomic, copy, nullable) void(^deviceUpdateProgress)(void);
 
 #pragma mark - V2-Action
 
 // 设备列表实例方法
 + (instancetype)modelWithDeviceInfo:(NSDictionary *)deviceInfo;
+// 更新设备信息
+- (void)updateDeviceInfo:(NSDictionary *)deviceInfo;
 // 更新设备时区
 - (void)updateTimeZone:(NSDictionary *)timeZoneDic;
 // 更新设备升级信息
@@ -103,7 +116,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)updateOnlineInfor:(NSDictionary *)onlineInfor;
 // 更新设备封面信息
 - (void)updateImageInfor:(NSDictionary *)imageInfo;
-
+// 更新设备完整信息
 - (void)updateDataWithNewDevice:(TGCameraDeviceModel *)device;
 
 @end
